dedf0cd54d28867fc7dbd7b72237d4a4e3a99bb2
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-29 13:00:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// write code here
+const sales = [
+  { price: 1000, discount: 0.1},
+  { price: 500, discount: 0.05},
+  { price: 100}
+];
+let summary = sales.map(sales =>{
+  if(sales.discount === undefined){
+    netPrice = sales.price
+  }else{
+    netPrice = sales.price - (sales.price * sales.discount)
+  }
+  return {netPrice : netPrice}
+})
+console.log(summary)
+// const summary = [
+//   {netPrice: 900},
+//   {netPrice: 475},
+//   {netPrice: 100}
+// ];
